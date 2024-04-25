96d9f5891c876ca26ac1bf9f644a44e205755e40
Satish <satishkumr001@gmail.com>
2024-04-25 17:47:43 +0530

    Format Demo class

--- instance,static/Demo.java
+++ instance,static/Demo.java
@@ -1,7 +1,7 @@
 class Demo{
-public static void main(String s[]){
-int z=Emp.add(10,30);
-System.out.println(z);
-System.out.println(Emp.cname);
+  public static void main(String s[]){
+  int z=Emp.add(10,30);
+  System.out.println(z);
+  System.out.println(Emp.cname);
 }
 }
